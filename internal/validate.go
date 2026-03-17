a7374032bcb79dccfbfcf5e03684f8148b7bf020
hamzaerrhh <hmzaeer@gmail.com>
2026-03-17 00:02:17 +0000

    structure the validation func

---
+++ internal/validate.go
@@ -0,0 +1,13 @@
+package internal
+
+import "fmt"
+
+func Validate(text string) {
+
+	fmt.Println("start validation")
+	fmt.Println("step 1 validation")
+	fmt.Println("calidate if there just blocks of 4 by 4 and contain just # adn .")
+	fmt.Println("step 2")
+	fmt.Println("validaet if there are conect (each one is conect even same x,same y or acrose) ")
+
+}
--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"tetrice/internal"
+)
 
 func main() {
 
 	fmt.Println("read the data and check args")
+	args:= os.Args
+	if len(args)!=2{
+		fmt.Println("the args are not set propaly")
+		return
+	}
+	files,err:=os.ReadFile(args[1])
+	if err!=nil{
+fmt.Println("error reading files")
+	}
+	fileContent:=string(files)
+	internal.Validate(fileContent)
+
 	fmt.Println("validate the tetrice ")
 	fmt.Println("optimize the tetrice")
 }
